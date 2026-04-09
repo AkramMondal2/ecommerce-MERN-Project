5be253ede06a1957e459f24fd59cfc6202caa895
AkramMondal2 <abcmondalxyz@gmail.com>
2026-04-09 14:01:14 +0530

    solve import PayPalButton

--- frontend/src/components/cart/Checkout.jsx
+++ frontend/src/components/cart/Checkout.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import PayPalButton from "./PayPalButton";
+import PayPalButton from "../cart/PaypalButton";
 
 const cart = {
   products: [
