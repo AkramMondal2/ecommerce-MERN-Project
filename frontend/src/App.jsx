d8c0c89f268376fcc515829f2d388e367762f8b4
AkramMondal2 <abcmondalxyz@gmail.com>
2026-04-09 11:44:38 +0530

    complete Admin ui part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,16 @@ import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import CollectionPage from "./pages/CollectionPage";
 import ProductDetails from "./components/products/ProductDetails";
+import Checkout from "./components/cart/Checkout";
+import OrderConfirmationPage from "./pages/OrderConfirmationPage";
+import OrderDetailsPage from "./pages/OrderDetailsPage";
+import MyOrdersPage from "./pages/MyOrdersPage";
+import AdminLayout from "./components/admin/AdminLayout";
+import AdminHomePage from "./pages/AdminHomePage";
+import UserManagement from "./components/admin/UserManagement";
+import ProductManagement from "./components/admin/ProductManagement";
+import EditProductPage from "./components/admin/EditProductPage";
+import OrderManagement from "./components/admin/OrderManagement";
 
 const App = () => {
   return (
@@ -20,8 +30,21 @@ const App = () => {
           <Route path="profile" element={<Profile />} />
           <Route path="collections/:collection" element={<CollectionPage />} />
           <Route path="product/:id" element={<ProductDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route
+            path="order-confirmation"
+            element={<OrderConfirmationPage />}
+          />
+          <Route path="order/:id" element={<OrderDetailsPage />} />
+          <Route path="my-orders" element={<MyOrdersPage />} />
         </Route>
-        <Route>{/*admin layout*/}</Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminHomePage />} />
+          <Route path="users" element={<UserManagement />} />
+          <Route path="products" element={<ProductManagement />} />
+          <Route path="products/:id/edit" element={<EditProductPage />} />
+          <Route path="orders" element={<OrderManagement />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/cart/Checkout.jsx
@@ -0,0 +1,222 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import PayPalButton from "./PayPalButton";
+
+const cart = {
+  products: [
+    {
+      name: "Stylish Jacket",
+      size: "M",
+      color: "Black",
+      price: 120,
+      image: "https://picsum.photos/150?random=1",
+    },
+    {
+      name: "Casual Sneakers",
+      size: "42",
+      color: "White",
+      price: 75,
+      image: "https://picsum.photos/150?random=2",
+    },
+  ],
+  totalPrice: 195,
+};
+
+const Checkout = () => {
+  const navigate = useNavigate();
+  const [checkoutId, setCheckoutId] = useState(null);
+  const [shippingAddress, setShippingAddress] = useState({
+    firstname: "",
+    lastname: "",
+    address: "",
+    city: "",
+    postalcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const handleChange = (e) => {
+    setShippingAddress({ ...shippingAddress, [e.target.name]: e.target.value });
+  };
+
+  const handleCreateCheckout = (e) => {
+    e.preventDefault();
+    setCheckoutId(123);
+    setShippingAddress({
+      firstname: "",
+      lastname: "",
+      address: "",
+      city: "",
+      postalcode: "",
+      country: "",
+      phone: "",
+    });
+  };
+
+  const handlePaymentSuccess = (details) => {
+    console.log("Payment Successfull", details);
+    navigate("/order-confirmation");
+  };
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto py-10 px-6 tracking-tighter">
+      {/* left section  */}
+      <div className="bg-white rounded-lg p-6">
+        <h2 className="text-2xl uppercase mb-6">Checkout</h2>
+        <form onSubmit={handleCreateCheckout}>
+          <h3 className="text-lg mb-4">Contact Details</h3>
+          <div className="mb-4">
+            <label className="block text-gray-700">Email</label>
+            <input
+              type="email"
+              value="user@example.com"
+              className="w-full p-2 border rounded"
+              disabled
+            />
+          </div>
+          <h3 className="text-lg mb-4">Delivery</h3>
+          <div className="mb-4 grid grid-cols-2 gap-4">
+            <div>
+              <label className="block text-gray-700">First Name</label>
+              <input
+                type="text"
+                name="firstname"
+                value={shippingAddress.firstname}
+                onChange={handleChange}
+                className="w-full p-2 border rounded "
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700">Last Name</label>
+              <input
+                type="text"
+                name="lastname"
+                value={shippingAddress.lastname}
+                onChange={handleChange}
+                className="w-full p-2 border rounded "
+                required
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Address</label>
+            <input
+              type="text"
+              name="address"
+              value={shippingAddress.address}
+              onChange={handleChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4 grid grid-cols-2 gap-4">
+            <div>
+              <label className="block text-gray-700">City</label>
+              <input
+                type="text"
+                name="city"
+                value={shippingAddress.city}
+                onChange={handleChange}
+                className="w-full p-2 border rounded "
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700">Postal Code</label>
+              <input
+                type="text"
+                name="postalcode"
+                value={shippingAddress.postalcode}
+                onChange={handleChange}
+                className="w-full p-2 border rounded "
+                required
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Country</label>
+            <input
+              type="text"
+              name="country"
+              value={shippingAddress.country}
+              onChange={handleChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Phone</label>
+            <input
+              type="tel"
+              name="phone"
+              value={shippingAddress.phone}
+              onChange={handleChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mt-6">
+            {!checkoutId ? (
+              <button
+                type="submit"
+                className="w-full bg-black text-white py-3 rounded"
+              >
+                Continue to Payment
+              </button>
+            ) : (
+              <div>
+                <h3 className="text-lg mb-4">Pay with Paypal</h3>
+                <PayPalButton
+                  amount={100}
+                  onSuccess={handlePaymentSuccess}
+                  onError={(err) => alert("Payment failed. Try again")}
+                />
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
+      {/* Right section  */}
+      <div className="bg-gray-50 p-6 rounded-lg">
+        <h3 className="text-lg mb-4">Order Summary</h3>
+        <div className="border-t py-4 mb-4">
+          {cart.products.map((product, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-between py-2 border-b"
+            >
+              <div className="flex items-start">
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="w-24 h-24 mr-4 object-cover"
+                />
+                <div>
+                  <h3 className="text-md">{product.name}</h3>
+                  <p className="text-gray-500">Size: {product.size}</p>
+                  <p className="text-gray-500">Color: {product.color}</p>
+                </div>
+              </div>
+              <p className="text-xl">₹{product.price?.toLocaleString()}</p>
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-between items-center text-lg mb-4">
+          <p>Subtotal</p>
+          <p>₹{cart.totalPrice?.toLocaleString()}</p>
+        </div>
+        <div className="flex justify-between items-center text-lg ">
+          <p>Shipping</p>
+          <p>Free</p>
+        </div>
+        <div className="flex justify-between items-center text-lg mb-4 pt-4 border-t">
+          <p>Total</p>
+          <p>₹{cart.totalPrice?.toLocaleString()}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
